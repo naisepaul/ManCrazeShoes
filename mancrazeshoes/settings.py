8d8f7427468cf338625e4c15e6f93c2ffc4deb93
Naise Paul <naisepaul@gmail.com>
2024-04-15 09:52:38 +0000

    Added home page content

--- mancrazeshoes/settings.py
+++ mancrazeshoes/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',   
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',  
     
 ]
 
