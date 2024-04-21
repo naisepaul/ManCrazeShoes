54f567ae372cf5ccd8aabe8092a64c6195c24bab
Naise Paul <naisepaul@gmail.com>
2024-04-21 12:35:59 +0000

    Customised admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import Product, ProductVariant, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(ProductVariant)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category','price']
+    list_filter = ['price','category']
+    search_fields = ['name','price']
+
+    ordering = ('-created_at',)
+
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ['product', 'size','stock']
+    list_filter = ['size','stock']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductVariant, ProductVariantAdmin)
 admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -24,7 +24,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=4, decimal_places=2)
     image = CloudinaryField('image')
-
+    created_at = models.DateTimeField(auto_now_add=True) 
+    
     def __str__(self):
         return self.name
 
