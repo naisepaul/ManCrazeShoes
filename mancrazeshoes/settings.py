4689c1ef5d7a4a1bdc824c79a854322cae27245d
Naise Paul <naisepaul@gmail.com>
2024-04-21 12:53:20 +0000

    Edited the Procfile

--- mancrazeshoes/settings.py
+++ mancrazeshoes/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR =  os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-naisepaul-mancrazeshoes-tgqsmquw52o.ws-eu110.gitpod.io']
 # ALLOWED_HOSTS = ['mancrazeshoes.herokuapp.com' , 'localhost']
