6257f59342ff79889a687bbbc296aab5be76c8e4
Naise Paul <naisepaul@gmail.com>
2024-05-01 22:25:49 +0000

    Added the shoe sizes to the product detail page

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Product, Category
+from .models import Product, Category, ProductVariant
 
 # Create your views here.
 
