0a97152ed9bb7bdfde70201fe682e4515e8a4dea
Naise Paul <naisepaul@gmail.com>
2024-05-04 18:57:17 +0000

    Added tool_bag templates for updating the subtotal

--- mancrazeshoes/settings.py
+++ mancrazeshoes/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIR,
-                # os.path.join(BASE_DIR, 'templates'),
+                os.path.join(BASE_DIR, 'templates'),
                 os.path.join(BASE_DIR, 'templates', 'allauth'),              
         ],
         'APP_DIRS': True,
