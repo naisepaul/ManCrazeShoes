5ab13efa0ef3d98c6df7611ab8ba4897f285863b
Naise Paul <naisepaul@gmail.com>
2024-05-04 00:44:05 +0000

    Finished the product size logic

--- bag/views.py
+++ bag/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+from products.models import Product, ProductVariant
 
 # Create your views here.
 
 def view_bag(request):
-    """ A view that renders the bag page """
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
@@ -11,13 +13,25 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
+    size = request.POST.get('size')    
     redirect_url = request.POST.get('redirect_url')
+    product = get_object_or_404(Product, pk=item_id)
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+    
