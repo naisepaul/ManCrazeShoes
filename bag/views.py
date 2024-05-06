37050bf57aca7666fe020c08e0356ece5535bb3c
Naise Paul <naisepaul@gmail.com>
2024-05-06 21:39:04 +0000

    Added message toasts to remove from_bag

--- bag/views.py
+++ bag/views.py
@@ -72,6 +72,8 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
+
+    product = get_object_or_404(Product, pk=item_id)
     try:
         size = request.POST.get('size') 
         bag = request.session.get('bag', {})
@@ -80,11 +82,14 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed size {size} {product.name} from your bag')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
