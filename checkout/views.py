49646856a817d16c30aae07899d4bcff8f9ee536
Naise Paul <naisepaul@gmail.com>
2024-05-08 22:08:11 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PEHP408EMVwNpSyNFpC98AB5zGwPPba8eQT6L3GUyLTinmwHPamzRyynJo4OkfCZKXEDGGrBXiqOTvAkJVEIFcW00wJdwhiN6',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
