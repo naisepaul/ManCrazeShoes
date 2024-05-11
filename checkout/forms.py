297f747f0d94db65a544e93b206d8ce11660d320
Naise Paul <naisepaul@gmail.com>
2024-05-11 12:34:00 +0000

    added styles to checkout forms

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,6 +3,9 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
+    """
+    Form for Order Model
+    """
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
@@ -16,28 +19,34 @@ class OrderForm(forms.ModelForm):
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
+
+        # Sets placeholder values for form inputs
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         # Sets autofocus on first input
         self.fields['full_name'].widget.attrs['autofocus'] = True
+
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            # Adds placeholders to inputs (except Country)
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
 
             # Adds styling classes to all inputs
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].widget.attrs['class'] = (
+                    'ib-form-field mb-3 px-2 py-2 font-body text-dark-grey')
+
             # Removes labels from inputs
             self.fields[field].label = False
