a2eb9bc63738f516fd5719650b9f4ace24ffdd2b
Naise Paul <naisepaul@gmail.com>
2024-05-14 16:11:02 +0000

    Added footer and change the product container color

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
+    
+    recent_items = Product.objects.order_by('-created_at')[:6]
+    return render(request, 'home/index.html', {'recent_items': recent_items})
 
-    return render(request, 'home/index.html')
+    
