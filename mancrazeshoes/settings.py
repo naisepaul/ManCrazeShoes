f8c9095bc97e6f1d5112bb77ad75c746b943ac5d
Naise Paul <naisepaul@gmail.com>
2024-07-02 21:50:37 +0000

    Changed debug to false

--- mancrazeshoes/settings.py
+++ mancrazeshoes/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-naisepaul-mancrazeshoes-5rgrp5zdqrx.ws.codeinstitute-ide.net',
